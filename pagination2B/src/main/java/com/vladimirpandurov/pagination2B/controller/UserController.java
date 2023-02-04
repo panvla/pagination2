7029ab99767311940c7c3dec159cf133cce43677
panvla <pandvlado@gmail.com>
2023-02-04 22:30:23 +0100

    asdf

--- pagination2B/src/main/java/com/vladimirpandurov/pagination2B/controller/UserController.java
+++ pagination2B/src/main/java/com/vladimirpandurov/pagination2B/controller/UserController.java
@@ -6,10 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.Map;
@@ -22,14 +19,14 @@ import java.util.Optional;
 public class UserController {
 
     private final UserService userService;
-
+    @GetMapping
     public ResponseEntity<HttpResponse> getUsers(@RequestParam Optional<String> name,
                                                  @RequestParam Optional<Integer> page,
                                                  @RequestParam Optional<Integer> size){
         return ResponseEntity.ok().body(
                 HttpResponse.builder()
                 .timeStamp(LocalDateTime.now().toString())
-                .data(Map.of("page", userService.getUsers(name.orElse(""), page.orElse(0), size.orElse(10))))
+                .data(Map.of("page", userService.getUsers(name.orElse(""), page.orElse(0), size.orElse(1))))
                 .message("Users Retrieved")
                 .status(HttpStatus.OK)
                 .statusCode(HttpStatus.OK.value())
